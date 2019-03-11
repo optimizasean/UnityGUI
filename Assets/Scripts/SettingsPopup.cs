555d5dc0ed301c15fab31ec40343397526447c30
optimizasean <optimizasean@gmail.com>
2019-03-11 11:45:46 -1000

    Added Functional open and close buttons, corrected earlier mistake on popup

---
+++ Assets/Scripts/SettingsPopup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsPopup : MonoBehaviour {
+    public void Open() {
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    public void Close() {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class UIController : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI scoreLabel;
+    [SerializeField] private SettingsPopup settingsPopup;
+
+    private void Start() {
+        settingsPopup.Close();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+    }
+
+    public void OnOpenSettings() {
+        settingsPopup.Open();
+    }
+}
